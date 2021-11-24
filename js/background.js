f58ce600d507a806ac46a809de55b76463c2de0e
Oleksandra9 <sashakrytska@gmail.com>
2021-11-24 17:19:34 +0000

    +color

---
+++ js/background.js
@@ -0,0 +1,8 @@
+const colors = ["tomato", "green", "orange", "yellow", "blue","red","gold","amber","pink","purple","black","brouwn"];
+const max = colors.length - 1;
+const min = 0;
+const index = Math.round(Math.random() * (max - min) + min);
+const color = colors[index];
+console.log(color);
+
+document.body.style.backgroundColor = color;
